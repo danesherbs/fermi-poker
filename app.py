c177bbbeea60afce16d7a07707ea31a5de44f19d
Dane <danesherbs@gmail.com>
2023-08-08 22:14:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -426,5 +426,5 @@ def get_state(game_id: str) -> Response:
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(host="0.0.0.0", port=5001, debug=False)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=False)
